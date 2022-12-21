cf792cdc2432f3ad8f2976d1cdd06b948e0c7576
Mohamed <mohamed.mekki@edu.ece.fr>
2022-12-21 22:41:40 +0100

    Fix:"adapt the layout with the new changes"

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,17 +1,30 @@
-
 import Header from '../components/Header.js'
 import Footer from '../components/Footer.js'
+import React, { useState,createContext} from 'react';
+
+
+export const AccentColorContext = createContext('blue');
 
 export default function Layout({
   children
 }){
+  const [color, setColor] = useState('white');
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  }
   return (
-    <div>
-      <Header />
-      <main className="py-10 min-h-screen max-w-full md:max-w-2xl md:mx-auto">
+    <AccentColorContext.Provider value={color}>
+    <div className={`bg-${color}-500 dark: bg-black`} >
+      <button className="bg-red-500" onClick={handleColorChange} value="red">Red</button>
+      <button className="bg-green-500" onClick={handleColorChange} value="green">Green</button>
+      <button className="bg-blue-500" onClick={handleColorChange} value="blue">Blue</button>
+      <button className="bg-yellow-500" onClick={handleColorChange} value="yellow">Yellow</button>
+    <Header />
+    <main className=" py-10 min-h-screen  max-w-full md:max-w-2xl md:mx-auto dark:text-white ">
       {children}
-      </main>
-      <Footer />
+    </main>
+    <Footer />
     </div>
+    </AccentColorContext.Provider>
   )
 }
