30349a2b2feedca21021d6c8990f848f12587145
Mohamed <mohamed.mekki@edu.ece.fr>
2022-12-21 23:06:23 +0100

    Fix(minor):"add color buttons"

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -6,9 +6,13 @@ export default function Layout({
   children
 }){
   return (
-    <div>
-      <Header />
-      <main className="py-10 min-h-screen max-w-full md:max-w-2xl md:mx-auto">
+    <div className={`bg-${color}-500 dark: bg-black`} >
+      <button className="bg-red-500" onClick={handleColorChange} value="red">Red</button>
+      <button className="bg-green-500" onClick={handleColorChange} value="green">Green</button>
+      <button className="bg-blue-500" onClick={handleColorChange} value="blue">Blue</button>
+      <button className="bg-yellow-500" onClick={handleColorChange} value="yellow">Yellow</button>
+    <Header />
+    <main className=" py-10 min-h-screen  max-w-full md:max-w-2xl md:mx-auto dark:text-white ">
       {children}
       </main>
       <Footer />
