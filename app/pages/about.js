a31928e628e2b629eedf14175ab1e2175f05b181
Mohamed <mohamed.mekki@edu.ece.fr>
2022-12-22 01:15:45 +0100

    feat:"changing the 'about us' page"

--- app/pages/about.js
+++ app/pages/about.js
@@ -1,20 +1,88 @@
 import Head from 'next/head'
 import Layout from '../components/Layout.js'
-
+import React from 'react';
 export default function About() {
+
   return (
-    <Layout>
+    <Layout >
       <Head>
-        <title>WebTech - about us</title>
+        <title>Readfine - about us</title>
+
         <meta name="description" content="WebTech about us page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 className='wt-title'>
         About us
       </h1>
-      <p>
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras sit amet enim sit amet nibh auctor interdum sit amet sed lacus. Nam consequat hendrerit lobortis. Mauris nec facilisis turpis. Nullam neque sem, ultricies in facilisis nec, rhoncus et nibh. Curabitur et risus in risus maximus scelerisque non at justo. Nullam blandit massa id justo elementum rutrum. Phasellus id facilisis lacus, vel dignissim magna. Duis at lacus a mi varius lacinia. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc ultrices justo bibendum aliquam tempus. Vivamus consequat metus eros, sit amet malesuada mi placerat a. Vivamus efficitur faucibus odio vel vulputate. Suspendisse eros dolor, pellentesque ac aliquam vel, rutrum eu ipsum. Donec quis nunc felis. 
-      </p>
+      
+
+
+      
+<p className="mb-3 font-light text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+<div className="grid grid-cols-1 md:gap-6 md:grid-cols-2">
+    <p className="mb-3 font-light text-gray-500 dark:text-gray-400">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+    <blockquote className="mb-3">
+        <p className="text-xl italic font-semibold text-gray-900 dark:text-white">" Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application. "</p>
+    </blockquote>
+</div>
+<p className="mb-3 font-light text-gray-500 dark:text-gray-400">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease, with a complete audit trail for every change.</p>
+
+
+<div className="grid mb-8 border border-gray-200 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2">
+    <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
+        <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
+            <p className="my-4 font-light">If you care for your time, I hands down would go with this."</p>
+        </blockquote>
+        <figcaption className="flex items-center justify-center space-x-3">
+            <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture"></img>
+            <div className="space-y-0.5 font-medium dark:text-white text-left">
+                <div>Bonnie Green</div>
+                <div className="text-sm font-light text-gray-500 dark:text-gray-400">Developer at Open AI</div>
+            </div>
+        </figcaption>    
+    </figure>
+    <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-tr-lg dark:bg-gray-800 dark:border-gray-700">
+        <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Solid foundation for any project</h3>
+            <p className="my-4 font-light">Designing with Figma components that can be easily translated to the utility classNamees of Tailwind CSS is a huge timesaver!"</p>
+        </blockquote>
+        <figcaption className="flex items-center justify-center space-x-3">
+            <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png" alt="profile picture"></img>
+            <div className="space-y-0.5 font-medium dark:text-white text-left">
+                <div>Roberta Casas</div>
+                <div className="text-sm font-light text-gray-500 dark:text-gray-400">Lead designer at Dropbox</div>
+            </div>
+        </figcaption>    
+    </figure>
+    <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-bl-lg md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
+        <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Mindblowing workflow</h3>
+            <p className="my-4 font-light">Aesthetically, the well designed components are beautiful and will undoubtedly level up your next application."</p>
+        </blockquote>
+        <figcaption className="flex items-center justify-center space-x-3">
+            <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="profile picture"></img>
+            <div className="space-y-0.5 font-medium dark:text-white text-left">
+                <div>Jese Leos</div>
+                <div className="text-sm font-light text-gray-500 dark:text-gray-400">Software Engineer at Facebook</div>
+            </div>
+        </figcaption>    
+    </figure>
+    <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-gray-200 rounded-b-lg md:rounded-br-lg dark:bg-gray-800 dark:border-gray-700">
+        <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Efficient Collaborating</h3>
+            <p className="my-4 font-light">You have many examples that can be used to create a fast prototype for your team."</p>
+        </blockquote>
+        <figcaption className="flex items-center justify-center space-x-3">
+            <img className="rounded-full w-9 h-9" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="profile picture"></img>
+            <div className="space-y-0.5 font-medium dark:text-white text-left">
+                <div>Joseph McFall</div>
+                <div className="text-sm font-light text-gray-500 dark:text-gray-400">CTO at Google</div>
+            </div>
+        </figcaption>    
+    </figure>
+</div>
+
     </Layout>
   )
 }
