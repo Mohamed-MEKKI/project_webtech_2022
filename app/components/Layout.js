f47cfee27ad0975e14413996191c263a4e0c5151
Mohamed <mohamed.mekki@edu.ece.fr>
2022-12-22 21:46:22 +0100

    fix:"change buttons"
    changing button for page theme color

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -8,6 +8,7 @@ export const AccentColorContext = createContext('blue');
 export default function Layout({
   children
 }){
+
   const [color, setColor] = useState('white');
   const handleColorChange = (event) => {
     setColor(event.target.value);
@@ -15,12 +16,12 @@ export default function Layout({
   return (
     <AccentColorContext.Provider value={color}>
     <div className={`bg-${color}-500 dark: bg-black`} >
-      <button className="bg-red-500" onClick={handleColorChange} value="red">Red</button>
-      <button className="bg-green-500" onClick={handleColorChange} value="green">Green</button>
-      <button className="bg-blue-500" onClick={handleColorChange} value="blue">Blue</button>
-      <button className="bg-yellow-500" onClick={handleColorChange} value="yellow">Yellow</button>
     <Header />
     <main className=" py-10 min-h-screen  max-w-full md:max-w-2xl md:mx-auto dark:text-white ">
+    <button  className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" onClick={handleColorChange} value="green">Green</button>
+    <button  className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" onClick={handleColorChange} value="red">Red</button>
+    <button  className="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-yellow-900" onClick={handleColorChange} value="yellow">Yellow</button>
+    <button  className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900" onClick={handleColorChange} value="purple">Purple</button>
       {children}
     </main>
     <Footer />
