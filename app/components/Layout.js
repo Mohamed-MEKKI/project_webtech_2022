5aa0bea60a1a00917b7b1bb070dcd63c09295526
Mohamed <mohamed.mekki@edu.ece.fr>
2022-12-23 02:14:10 +0100

    feat:"Layout changes"
    resolve the conflict between our dark mode and color switch

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -14,16 +14,19 @@ export default function Layout({
   }
   return (
     <AccentColorContext.Provider value={color}>
-    <div className={`bg-${color}-500 dark: bg-black`} >
-      <button className="bg-red-500" onClick={handleColorChange} value="red">Red</button>
+    <div className={`bg-${color}-500`} >
+      <div className='dark:bg-black'>
+    
+    <Header />
+    <main className=" py-10 min-h-screen  max-w-full md:max-w-2xl md:mx-auto dark:text-white ">
+    <button className="bg-red-500" onClick={handleColorChange} value="red">Red</button>
       <button className="bg-green-500" onClick={handleColorChange} value="green">Green</button>
       <button className="bg-blue-500" onClick={handleColorChange} value="blue">Blue</button>
       <button className="bg-yellow-500" onClick={handleColorChange} value="yellow">Yellow</button>
-    <Header />
-    <main className=" py-10 min-h-screen  max-w-full md:max-w-2xl md:mx-auto dark:text-white ">
       {children}
     </main>
     <Footer />
+    </div>
     </div>
     </AccentColorContext.Provider>
   )
