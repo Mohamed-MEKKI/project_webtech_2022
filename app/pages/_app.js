7acd2d0972be6023ee8d94fdbb9e1f1b09e1decc
ThanthaiU <112929873+ThanthaiU@users.noreply.github.com>
2022-12-24 17:31:50 +0100

    fix: update _app.js

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -6,11 +6,11 @@ import { UserContextProvider} from '../components/UserContext'
 import { ThemeContextProvider } from '../components/ThemeContext'
 
 export default function MyApp({ Component, pageProps }) {
-  // const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
   return (
     <ThemeContextProvider>
       <SessionContextProvider
-        // supabaseClient={supabaseClient}
+        supabaseClient={supabaseClient}
         initialSession={pageProps.initialSession}
       >
         <UserContextProvider>
